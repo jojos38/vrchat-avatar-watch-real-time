4b65e26cadacfaecf3b63c241693bfeff9087f33
jojos38 <jojos38000@hotmail.com>
2022-12-12 21:50:17 +0100

    All assets

---
+++ OSC program/index.js
@@ -0,0 +1,124 @@
+const { Client, Server } = require('node-osc');
+
+class WatchOSC {
+	// VRChat sending port and IP
+	#VRCHAT_PORT = 9000;
+	#VRCHAT_HOST = '127.0.0.1';
+	
+	// OSC data receiving port and IP
+	#THIS_PORT = 9001;
+	#THIS_HOST = '0.0.0.0';
+	
+	// IO
+	#client;
+	#server;
+	
+	// OSC stuff
+	#avatarId = 'avtr_bd2046ff-5206-44df-8bc1-1ed5e408ef64';
+	#hoursPath = '/avatar/parameters/HandWatchHours';
+	#minutesPath = '/avatar/parameters/HandWatchMinutes';
+	
+	// Auto shutdown
+	#shutdownCheckTimer = 1000; // 1 second
+	#stopSendingDataAfter = 3 * 60 * 1000; // 10 minutes
+	#lastReceivedData = 0;
+	#sendData = true;
+
+	// Time
+	static #MULTIPLIER = 0.0078125;
+	#lastHours;
+	#lastMinutes;
+
+	/**
+	 * Opens server and prepare client
+	 */
+	constructor() {
+		this.#reset();
+		this.#client = new Client(this.#VRCHAT_HOST, this.#VRCHAT_PORT);
+		this.#server = new Server(this.#THIS_PORT, this.#THIS_HOST, this.#onServerReady.bind(this));
+		this.#server.on('message', this.#onMessageReceive.bind(this));
+		setInterval(this.#sendTime.bind(this), 1000);
+		setInterval(this.#checkShutdown.bind(this), this.#shutdownCheckTimer);
+	}
+
+	/**
+	 * Resets minutes and hours effectively re-sending them
+	 */
+	#reset() {
+		this.#lastHours = -1;
+		this.#lastMinutes = -1;
+	}
+
+	/**
+	 * Checks every x if a data was received from VRChat, if no data was received for more than 3 minutes
+	 * We stop sending time packets because the game is likely not running anymore.
+	 *
+	 * Note: We do receive our own messages so SendTime() acts as a ping
+	 */
+	#checkShutdown() {
+		this.#lastReceivedData += this.#shutdownCheckTimer;
+		if (this.#lastReceivedData > this.#stopSendingDataAfter && this.#sendData) {
+			console.log('No data received for 10 seconds, stopping data sending');
+			this.#sendData = false;
+		}
+		else if (this.#lastReceivedData < this.#stopSendingDataAfter && !this.#sendData) {
+			console.log('Data received, starting data sending');
+			this.#sendData = true;
+		}
+	}
+
+	/**
+	 * Called when the OSC server is ready to receive data
+	 */
+	#onServerReady() {
+		console.log('OSC server ready and listening on ' + this.#THIS_HOST + ':' + this.#THIS_PORT);
+	}
+
+	/**
+	 * Called when VRChat sends a message
+	 * @param {array.<String>} message The message sent from VRChat
+	 */
+	#onMessageReceive(message) {
+		const path = message[0];
+		const data = message[1];
+		if (path === '/avatar/change' && data === this.#avatarId) {
+			console.log('Avatar was loaded, re-sending watch time');
+			this.#reset();
+		}
+
+		this.#lastReceivedData = 0;
+	}
+
+	/**
+	 * Sends the hours and the minutes to the watch in VRChat using OSC
+	 * This function is called regularly but only sends data when there was a change
+	 */
+	#sendTime() {
+		if (!this.#sendData) return;
+
+		const currentTime = new Date();
+		const hours = currentTime.getHours();
+		const minutes = currentTime.getMinutes();
+
+		// Check if hours needs to be sent
+		if (hours !== this.#lastHours) {
+			let floatValue = hours * WatchOSC.#MULTIPLIER;
+			if (floatValue <= 0.001) floatValue = 0.001; // VRChat considers 0 as an int and refuses it, this is a workaround
+			this.#client.send(this.#hoursPath, floatValue);
+			console.log('Hours set to ' + hours.toString().padStart(2, '0'));
+		}
+
+		// Check if minutes needs to be sent
+		if (minutes !== this.#lastMinutes) {
+			let floatValue = minutes * WatchOSC.#MULTIPLIER;
+			if (floatValue <= 0.001) floatValue = 0.001; // VRChat considers 0 as an int and refuses it, this is a workaround
+			this.#client.send(this.#minutesPath, floatValue);
+			console.log('Minutes set to ' + minutes.toString().padStart(2, '0'));
+		}
+
+		this.#lastHours = hours;
+		this.#lastMinutes = minutes;
+	}
+}
+
+WatchOSC = new WatchOSC();
---
+++ Shader/Watch.shader
@@ -0,0 +1,139 @@
+﻿// Created by Neitri, edited by jojos38 free of charge, free to redistribute
+
+Shader "jojos38/NeitriWatch" {
+	Properties {
+		_MainTex ("_MainTex", 2D) = "white" {}
+		_Color("_Color", Color) = (1,1,1,1)
+		[FloatRange]_Brightness("_Brightness", Range(0, 1)) = 1
+		[HideInInspector]_RealTimeHours("Real Time Hours", Float) = 1
+		[HideInInspector]_RealTimeMinutes("Real Time Minutes", Float) = 1
+	}
+	SubShader {
+		Tags {
+			"Queue" = "Transparent"
+			"RenderType" = "Transparent"
+		}
+		LOD 100
+		Cull Off
+		Blend SrcAlpha OneMinusSrcAlpha // transparent
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile_fog // make fog work
+			
+			#include "UnityCG.cginc"
+
+			struct appdata {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				float4 color : COLOR;
+			};
+
+			struct v2f {
+				float4 vertex : SV_POSITION;
+				float3 uv : TEXCOORD0;
+				float4 color : COLOR;
+				UNITY_FOG_COORDS(1)
+			};
+
+			uniform float _RealTimeHours;
+			uniform float _RealTimeMinutes;
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float4 _Color;
+			float _Brightness;
+
+			v2f vert (appdata v) {
+				v2f o;
+				o.uv.xy = TRANSFORM_TEX(v.uv, _MainTex);
+				o.uv.z = 0;
+				o.color = _Color * _Brightness;
+
+				uint s1 = floor(_Time.y);
+				uint s10 = floor(s1 / 10);
+				uint m1 = floor(s10 / 6);
+				uint m10 = floor(m1 / 10);
+				uint h1 = floor(m10 / 6);
+				uint h10 = floor(h1 / 10);
+
+				uint minutes60 = int(_RealTimeMinutes / 0.0078125f);
+				uint minutesL = int(minutes60 % 10);
+				uint minutesR = int(minutes60 / 10);
+
+				uint hours60 = int(_RealTimeHours / 0.0078125f);
+				uint hoursL = int(hours60 % 10);
+				uint hoursR = int(hours60 / 10);
+
+				s1 -= s10 * 10;
+				s10 -= m1 * 6;
+				m1 -= m10 * 10;
+				m10 -= h1 * 6;
+				h1 -= h10 * 10;
+
+				if (v.color.r == 0) {
+					if (v.color.g == 0) {
+						if (v.color.b == 0) {
+							// seconds
+							o.uv.x += s1 / 10.0f;
+						} else {
+							// 10 seconds
+							o.uv.x += s10 / 10.0f;
+						}
+					} else {
+						if (v.color.b == 0) {
+							// minutes
+							// o.uv.x += m1 / 10.0f;
+							o.uv.x += minutesL / 10.0f;
+						} else {
+							// 10 minutes
+							// o.uv.x += m10 / 10.0f;
+							o.uv.x += minutesR / 10.0f;
+						}
+					}
+				} else if (v.color.r == 1) {
+					if (v.color.g == 0) {
+						if (v.color.b == 0) {
+							// hours
+							// o.uv.x += h1 / 10.0f;
+							o.uv.x += hoursL / 10.0f;
+						} else {
+							// 10 hours
+							// o.uv.x += h10 / 10.0f;
+							o.uv.x += hoursR / 10.0f;
+						}
+					} else {
+						uint fps1 = unity_DeltaTime.w;
+						uint fps10 = floor(fps1 / 10);
+						fps1 -= fps10 * 10;
+						if (v.color.b == 0) {
+							// fps
+							o.uv.x += fps1 / 10.0f;
+						} else {
+							// 10 fps
+							o.uv.x += fps10 / 10.0f;
+						}
+					}
+				}
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// Number quad
+				fixed4 color = tex2D(_MainTex, i.uv);
+				clip(color.a - 0.1);
+				color *= i.color;
+
+				// Apply fog
+				UNITY_APPLY_FOG(i.fogCoord, color);
+				return color;
+			}
+			ENDCG
+		}
+	}
+}
